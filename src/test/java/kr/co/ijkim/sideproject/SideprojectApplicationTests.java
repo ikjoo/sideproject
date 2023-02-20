e0d8d7737a047c6a3717bda02f271fb884edc0a6
ikjoo kim <fhdhaldh@naver.com>
2023-02-20 16:00:19 +0900

    first commit

---
+++ src/test/java/kr/co/ijkim/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ijkim.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
