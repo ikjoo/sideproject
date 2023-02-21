2b4f8fe94975fe81dcb7c1b56a8f6bdf9c25c7a7
ijkim <ijkim@kanak.co.kr>
2023-02-21 23:15:55 +0900

    자바 버전 11로 변경, 부트 버전 2.7.1로 변경,user entity 생성

---
+++ src/main/java/kr/co/ijkim/sideproject/user/model/entity/User.java
@@ -0,0 +1,30 @@
+package kr.co.ijkim.sideproject.user.model.entity;
+
+import kr.co.ijkim.sideproject.common.EntityBase;
+import lombok.Getter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@DynamicInsert
+@DynamicUpdate
+public class User extends EntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
+    @Column(length = 30, nullable = false, unique = true)
+    private String userId;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @Column(length = 50, nullable = false)
+    private String email;
+    @Column(length = 10, nullable = false)
+    private String userName;
+    @Column(length = 20, nullable = false)
+    private String phoneNumber;
+
+
+}
